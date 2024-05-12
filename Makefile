9140272524b144e17d395347119d11d1d9109be2
NAKAJIMA Yusaku <yupong7@gmail.com>
2024-05-12 17:41:05 +0900

    Implement getuser2

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ TARGET = getuser
 SRCS = getuser.cpp getuser2.cpp
 OBJS = $(SRCS:.cpp=.o)
 
+CXXFLAGS = -std=c++20
+
 all: $(TARGET)
 clean:
 	- rm -f $(TARGET) $(OBJS)
--- getuser2.cpp
+++ getuser2.cpp
@@ -1,5 +1,15 @@
 #include "getuser.h"
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
 string getuser2() {
-    return "";
+    // struct passwd 
+    auto *p = getpwuid(getuid());
+    if (p == NULL) {
+        // errno
+    }
+
+    return p->pw_name;
 }
